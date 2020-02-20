5ffc290429e9a1cc8e7e8d0141a254d42a09e610
GiSpatial Technology Malaysia <github@gispatial.tech>
2020-02-20 16:39:09 +0500

    showing user email on top nav

--- ClientApp/src/app/secure/secure.component.ts
+++ ClientApp/src/app/secure/secure.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
-import { AuthService } from '../services/auth.service';
+import { AuthService, Session } from '../services/auth.service';
 @Component({
   selector: 'app-secure',
   templateUrl: './secure.component.html',
   styleUrls: ['./secure.component.scss']
 })
 export class SecureComponent implements OnInit {
-
+  currentUser: Session = null;
   constructor(
     private router: Router,
     private authService: AuthService,
-  ) { }
+  ) {
+    this.currentUser = authService.getCurrentSession()
+  }
 
   ngOnInit(): void {
   }
